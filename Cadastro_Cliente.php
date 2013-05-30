26dffcaea3c7aeeb39a187641f273432f26b2daa
Thiago Moreira <thiago.samuel@gmail.com>
2013-05-30 20:53:53 -0300

    Commit inicial. Criacao de 2 arquivos

---
+++ Cadastro_Cliente.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Cadastro de Cliente</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<p>Segunda página de teste para o git.</p>
+</body>
+</html>
